139c956147d029a6e5197cebadd547737c745500
edison redrovan <edison redrovan@DESKTOP-OV0UKDS>
2018-10-03 13:39:24 -0500

    serialisable

--- VentaServicios/src/main/java/physeter/ventaservicios/modelo/Servicio.java
+++ VentaServicios/src/main/java/physeter/ventaservicios/modelo/Servicio.java
@@ -1,5 +1,7 @@
 package physeter.ventaservicios.modelo;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Servicio {
+public class Servicio implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
