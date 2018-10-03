646533578f0e72b37b40abd51d8e82ae96dfb275
edison redrovan <edison redrovan@DESKTOP-OV0UKDS>
2018-10-03 11:53:34 -0500

    pesona campos

--- VentaServicios/src/main/java/physeter/ventaservicios/modelo/Persona.java
+++ VentaServicios/src/main/java/physeter/ventaservicios/modelo/Persona.java
@@ -1,5 +1,20 @@
 package physeter.ventaservicios.modelo;
 
 public class Persona {
+	
+	
+	private int id;
+	
+	private String cedula;
+	
+	private String nombre;
+	
+	private String foto;
+	
+	private String telefono;
+	
+	private String correo;
+	
+	private String contraseña;
 
 }
