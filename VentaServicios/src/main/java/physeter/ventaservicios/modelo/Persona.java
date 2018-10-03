d856feedd22986edbf5646194c6014010847e99b
“plemasa” <pblandreslema@gmail.com>
2018-10-03 11:54:07 -0500

    codePersona

--- VentaServicios/src/main/java/physeter/ventaservicios/modelo/Persona.java
+++ VentaServicios/src/main/java/physeter/ventaservicios/modelo/Persona.java
@@ -1,5 +1,24 @@
 package physeter.ventaservicios.modelo;
 
-public class Persona {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Persona implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "PER_ID")
+	private int id;
+	
+	@Column(name = "PER_NOMBRE")
+	private String nombre;
 
 }
